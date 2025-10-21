58f66240e1ecd0789875dd576afcaba542f16016
Neel Patel <neelpp@email.sc.edu>
2025-10-21 12:56:02 -0400

    example

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './css/index.css';
+import Layout from './Layout';
 import Home from './pages/Home';
+import About from './pages/About';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path="about" element={<About />}/>
+        </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
-<React.StrictMode>
-    <h1>Hi Neel</h1>
-    <h2>Loving this</h2>
-    <Home />
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
