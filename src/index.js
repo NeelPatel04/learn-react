aeeba00a8d8b4cf02e063f711560d6fef98029c4
Neel Patel <neelpp@email.sc.edu>
2025-10-24 22:28:26 -0400

    main page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './css/index.css';
 import Layout from './Layout';
 import Home from './pages/Home';
 import About from './pages/About';
+import Houses from './pages/Houses';
+import Contact from './pages/Contact';
 
 export default function App() {
   return (
-    <BrowserRouter>
-    <Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
           <Route path="about" element={<About />}/>
+          <Route path="houses" element={<Houses />}/>
+          <Route path="contact" element={<Contact />}/>
         </Route>
-    </Routes>
+      </Routes>
     </BrowserRouter>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,9 @@ import "./../css/Home.css";
 
 const Home = () => {
     return (
-        <div id="home">
-            <h1>I love home</h1>
-            <h2>Home is for the heart</h2>
-        </div>
+        <main id="home" className="main-content">
+            <h2>Home</h2>
+        </main>
     );
 };
 
